2fd5fa5d88534ad0cf89f7bd9444e894adaefff2
Habibullah <habibullahhisam@gmail.com>
2025-04-30 11:52:06 +0600

    Change cursor design and interaction.

---
+++ scripts/cursor.js
@@ -0,0 +1,51 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+const cursorAnotherOutline = document.querySelector("[data-another-cursor-outline]");
+
+window.addEventListener("mousemove", function(e) {
+  
+  const posX = e.clientX;
+  const posY = e.clientY;
+  
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+  
+  // cursorOutline.style.left = `${posX}px`;
+  // cursorOutline.style.top = `${posY}px`;
+  
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, {
+    duration: 500, fill: "forwards"
+  });
+  cursorAnotherOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, {
+    duration: 1000, fill: "forwards"
+  });
+  
+});
+
+window.addEventListener("click", function (e){
+  
+  cursorOutline.style.height = `20px`;
+  cursorOutline.style.width = `20px`;
+  
+  // cursorAnotherOutline.style.height = `30px`;
+  // cursorAnotherOutline.style.width = `30px`;
+  
+  setTimeout(() => {
+    onClick();
+  }, 100);
+  
+});
+
+function onClick(){
+  cursorOutline.style.height = `30px`;
+  cursorOutline.style.width = `30px`;
+  
+  // cursorAnotherOutline.style.height = `45px`;
+  // cursorAnotherOutline.style.width = `45px`;
+}
