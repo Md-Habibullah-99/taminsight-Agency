e0dd819f520a5edeff78e28cce0c47834b40ff4e
md-habibullah-99 <habibullahhisam@gmail.com>
2025-10-04 13:41:23 +0600

    Apply css code to affiliate-here.

--- src/components/Pages/Affiliate.jsx
+++ src/components/Pages/Affiliate.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../main_components/Footer";
 import AffiliateModel from "../affiliate_components/AffiliateModel";
+import AffiliateHero from "../affiliate_components/AffiliateHero";
 
 
 function Affiliate() {
   return (
     <>
+      <AffiliateHero />
       <AffiliateModel />
       <Footer />
     </>
---
+++ src/components/affiliate_components/AffiliateHero.jsx
@@ -0,0 +1,22 @@
+
+
+export default function AffiliateHero() {
+  return (
+    <>
+      <div id="delete-vars-btn" className="hero_header is_affiliate">
+          <h1 className="work_areas">
+            <div className="gradient-brand">Join the Forces of</div>
+
+              <div className="headlng-gradient">Taminsight Affiliates</div>
+            {/* <span className="work_areas_list">
+              <div className="headlng-gradient">Experience</div>
+
+            </span>
+            <span className="work_areas_list">
+              <div className="headlng-gradient">Delivered</div>
+            </span> */}
+            </h1>
+      </div>
+    </>
+  )
+}
