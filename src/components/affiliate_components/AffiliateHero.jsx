0e058600b62b2642731f1cbe00c85ec30ab5b06d
md-habibullah-99 <habibullahhisam@gmail.com>
2025-10-14 23:22:16 +0600

    refactor: enhance AffiliateHero component styling and layout for better responsiveness.

--- src/components/affiliate_components/AffiliateHero.jsx
+++ src/components/affiliate_components/AffiliateHero.jsx
@@ -1,33 +1,23 @@
 
 
 import { withBase } from "../../utils/withBase";
-
 import BecomeAffiliateButton from "../sub_components/BecomeAffiliateButton";
 
 export default function AffiliateHero() {
-
   return (
     <>
       <div id="delete-vars-btn" className="hero_header is_affiliate">
         <div className="hero_header_title-container">
-          <div className="affiliate_hero-title-mini">
-            SHARE, GET PAID, REPEAT
-          </div>
+          <div className="affiliate_hero-title-mini">SHARE, GET PAID, REPEAT</div>
           <h1 className="work_areas">
             <div className="gradient-brand">Join the Forces of</div>
-
             <div className="headlng-gradient">Taminsight Affiliates</div>
-
           </h1>
           <div className="affiliate_hero-title-mini">
             Become our affiliate to enjoy exclusive perks, dedicated support, and unlimited earning potential!
           </div>
-            <BecomeAffiliateButton />
-          
+          <BecomeAffiliateButton />
         </div>
-        {/* <div className="affiliate-image-outer-container">
-            <img src={withBase('/images/cats/TheWahCat.jpg')} alt="" className="affiliate-hero-image" />
-        </div> */}
       </div>
     </>
   )
