99679ea8505146dd01c93c5b9ef0e96d25c0446e
md-habibullah-99 <habibullahhisam@gmail.com>
2025-11-26 20:04:58 +0600

    Fix some codes.

--- src/components/partnership_components/PartnershipBenifits.jsx
+++ src/components/partnership_components/PartnershipBenifits.jsx
@@ -13,15 +13,15 @@ export default function PartnershipBenifits() {
     {
       title: "Get a Free Custom-Built Website *",
       frequency: "Stay in control and monitor your projects effortlessly with a swanky website for completely FREE",
-      imgLink: "../../../public/images/cats/cat02.jpg",
-      imgPlace: "1",
+      imgLink: "https://cdn.prod.website-files.com/65111bc3d36d02e461763c5f/655097f7746087a0082c2a85_fast-work.json",
+      maxElement: false,
       features: [],
     },
     {
       title: "Daily Report",
       frequency: "Stay effortlessly informed with our accessible Daily Report",
-      imgLink: "../../../public/images/cats/cat02.jpg",
-      imgPlace: "1",
+      imgLink: "https://cdn.prod.website-files.com/65111bc3d36d02e461763c5f/655097f7746087a0082c2a85_fast-work.json",
+      maxElement: false,
       features: [
         "Access to real-time reports <br />",
         "Monitor and track your progress <br />",
@@ -30,8 +30,8 @@ export default function PartnershipBenifits() {
     {
       title: "Dedicated Team",
       frequency: "Elevate your project's success with an exclusive dedicated team, customized JUST FOR YOU!",
-      imgLink: "../../../public/images/cats/cat02.jpg",
-      imgPlace: "2",
+      imgLink: "https://cdn.prod.website-files.com/65111bc3d36d02e461763c5f/655097f7746087a0082c2a85_fast-work.json",
+      maxElement: true,
       features: [
         "Team solely focused on your project <br />",
         "Skilled project manager <br />",
@@ -41,8 +41,8 @@ export default function PartnershipBenifits() {
     {
       title: "Collaborative Growth",
       frequency: "Unleash your business's potential with collaborative growth",
-      imgLink: "../../../public/images/cats/cat02.jpg",
-      imgPlace: "2",
+      imgLink: "https://cdn.prod.website-files.com/65111bc3d36d02e461763c5f/655097f7746087a0082c2a85_fast-work.json",
+      maxElement: true,
       features: [
         "24/7 team support <br />",
         "Comprehensive marketing strategies <br />",
@@ -51,8 +51,8 @@ export default function PartnershipBenifits() {
     {
       title: "Enrollment for FREE",
       frequency: "Join as our partner and start earning",
-      imgLink: "../../../public/images/cats/cat02.jpg",
-      imgPlace: "1",
+      imgLink: "https://cdn.prod.website-files.com/65111bc3d36d02e461763c5f/655097f7746087a0082c2a85_fast-work.json",
+      maxElement: false,
       features: [
         "$0 enrollment fee <br />",
         "Six star service <br />",
@@ -62,8 +62,8 @@ export default function PartnershipBenifits() {
     {
       title: "Steady Profit",
       frequency: "Maximize your earnings with effortless recurring profits!",
-      imgLink: "../../../public/images/cats/cat02.jpg",
-      imgPlace: "1",
+      imgLink: "https://cdn.prod.website-files.com/65111bc3d36d02e461763c5f/655097f7746087a0082c2a85_fast-work.json",
+      maxElement: false,
       features: [
         "Earn up to 40% of project earnings <br />",
         "Unlock unlimited growth <br />",
@@ -76,41 +76,41 @@ export default function PartnershipBenifits() {
 
   return (
     <>
-          <div className="affiliate-program affiliate-program_container" id="program">
-            {/* color gradient */}
-            <div className="affiliate_program__bg"></div>
-            <div className="global-padding global-padding-section">
-              <div className="container-large">
-                <div className="section-header">
-                  <div className="section_header_wrapper">
-                    <div data-scroll-reveal="" className="text-eyebrow is-process">
-                      <div>BENEFITS</div>
-                    </div>
-                    <h1 data-scroll-reveal="" className="heading-gradient is-bg-cream"><span className="newline">Our Partnership Benefits
-                    </span><span className="newline">That You’ll Love</span>
-                    </h1>
-                  </div>
+      <div className="affiliate-program affiliate-program_container" id="program">
+        {/* color gradient */}
+        <div className="affiliate_program__bg"></div>
+        <div className="global-padding global-padding-section">
+          <div className="container-large">
+            <div className="section-header">
+              <div className="section_header_wrapper">
+                <div data-scroll-reveal="" className="text-eyebrow is-process">
+                  <div>BENEFITS</div>
                 </div>
-    
-                <div className="benefits-cart-container">
-                  {cards.map((c, idx) => (
-                    <Benifit_Card
-                      key={idx}
-                      title={c.title}
-                      frequency={c.frequency}
-                      features={c.features}
-                      imgLink={c.imgLink}
-                      imgPlace={c.imgPlace}
-                    />
-                  ))}
-                </div>
-    
-              </div>
-              <div className="affiliate-partner-bottom-button-container">
-                <BecomeSoemthingButton />
+                <h1 data-scroll-reveal="" className="heading-gradient is-bg-cream"><span className="newline">Our Partnership Benefits
+                </span><span className="newline">That You’ll Love</span>
+                </h1>
               </div>
             </div>
+
+            <div className="benefits-cart-container">
+              {cards.map((c, idx) => (
+                <Benifit_Card
+                  key={idx}
+                  title={c.title}
+                  frequency={c.frequency}
+                  features={c.features}
+                  imgLink={c.imgLink}
+                  maxElement={c.maxElement}
+                />
+              ))}
+            </div>
+
           </div>
-        </>
+          <div className="affiliate-partner-bottom-button-container">
+            <BecomeSoemthingButton />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
