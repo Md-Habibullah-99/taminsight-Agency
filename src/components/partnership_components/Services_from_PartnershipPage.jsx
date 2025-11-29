6fe2c0564b9090539747d404647f2f34af036b82
md-habibullah-99 <habibullahhisam@gmail.com>
2025-11-29 22:37:31 +0600

    Add Grab_Value component and integrate into services and partnership sections.

--- src/components/partnership_components/Services_from_PartnershipPage.jsx
+++ src/components/partnership_components/Services_from_PartnershipPage.jsx
@@ -1,17 +1,25 @@
 
 
+import Grab_Value from "../sub_components/Grab_Value";
+
 export default function Services({servicesArr = []}) {
   return (
-    <div className="services_line-container-main">
-      {servicesArr.map((service, idx) => (
-        <div key={idx} className="benefit-cart-top-elements services_line-container">
-          <h5 className="benefit-card-max-element">{service.title}</h5>
-          <h5 className="services_line-price">{service.price}</h5>
-        </div>
-      ))}
-      
+    <div className="services_mini_container">
+      <div className="services_line-container-main">
+        {servicesArr.map((service, idx) => (
+          <div key={idx} className="benefit-cart-top-elements services_line-container">
+            <h5 className="benefit-card-max-element">{service.title}</h5>
+            <h5 className="services_line-price">{service.price}</h5>
+          </div>
+        ))}
+        
+      </div>
       <div className="partner-services_bottom-value">
-        <h2>Total Free Value <span className="price">$3000</span></h2>
+        <div>Total Free Value <span className="price">$3000</span>
+          <a href="https://www.guthib.com">
+            <Grab_Value content={["Grab Your", "Offer"]} />
+          </a>
+        </div>
       </div>
     </div>
   );
